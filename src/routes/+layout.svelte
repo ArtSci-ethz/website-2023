d7581056bd8e9984dbb83b2234e67b5312fa82dc
Felix Hennig <mail@felixhennig.com>
2022-12-31 13:35:26 +0100

    feature: better header bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script>
   import '../app.css';
+  import logo from '$lib/assets/logo.svg';
 </script>
-
-<div class="border border-red-500">This is appearing on every page</div>
+<!-- Header for all pages -->
+<div class="flex flex-row m-4 items-center">
+  <!-- Logo -->
+  <img alt="The ArtSci logo" src="{logo}" />
+  <!-- Menu -->
+  <div class="ml-auto">
+    <a href="/">Index</a>
+    <a href="/about">About</a>
+  </div>
+</div>
 
 <slot />
+
+<style lang="postcss">
+  a {
+    @apply underline
+  }
+</style>
