9ba976c5124b0d3bb30db5919f9b44a2ea742f90
Felix Hennig <mail@felixhennig.com>
2023-01-01 16:34:46 +0100

    style: footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="text-white flex flex-row mt-4 px-4 py-2">
+<footer class="text-white flex flex-row mt-4 px-4 py-6 bg-[#6f735f]">
   <div>© ArtSci 2023</div>
   <div class="flex-1" />
   <div class="px-2">
@@ -12,8 +12,8 @@
   </div>
 </footer>
 
-<style>
-  footer {
-    background-color: #6f735f;
+<style lang="postcss">
+  a {
+    @apply font-bold underline;
   }
 </style>
