fdad4298c90d15362b61d0150e3c45c62eec173f
Felix Hennig <mail@felixhennig.com>
2023-01-01 15:58:38 +0100

    feat: prettier menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import MenuLink from './MenuLink.svelte';
   import logo from '$lib/assets/logo.svg';
 
   export let backgroundImage: string | undefined;
   export let text: string | undefined;
 </script>
 
-<header class="w-full h-80 p-4 bg-cover" style="background-image: url({backgroundImage});">
+<header class="w-full h-96 p-12 bg-cover" style="background-image: url({backgroundImage});">
   <div class="flex flex-row justify-between">
     <!-- Logo -->
     <div>
@@ -13,12 +14,12 @@
     </div>
 
     <!-- Menu -->
-    <div>
-      <a href="/">About</a>
-      <a href="/visit">Visit</a>
-      <a href="/participate">Participate</a>
-      <a href="/past">Past Exhibitions</a>
-      <a href="/join-and-give">Join & Give</a>
+    <div class="ml-8 w-8/12 flex flex-row justify-between items-end">
+      <MenuLink href="/" text="About" />
+      <MenuLink href="/visit" text="Visit" />
+      <MenuLink href="/participate" text="Participate" />
+      <MenuLink href="/past" text="Past Exhibitions" />
+      <MenuLink href="/join-and-give" text="Join & Give" />
     </div>
   </div>
 
@@ -27,10 +28,3 @@
     {text}
   </div>
 </header>
-
-<style>
-  .bg {
-    width: 100%;
-    height: 300px;
-  }
-</style>
---
+++ src/lib/components/MenuLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  export let href: string;
+  export let text: string;
+
+  let isCurrent: boolean;
+
+  page.subscribe(value => {
+    isCurrent = $page.url.pathname === href;
+  })
+</script>
+
+{#if isCurrent}
+<a class="text-black font-bold" href={href}>{text}</a>
+{:else}
+<a class="text-slate-600" href={href}>{text}</a>  
+{/if}
