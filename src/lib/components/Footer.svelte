a6deb0928eedd58746da9e2842709ba8f002b771
Chaoran Chen <mail@chaoran-chen.de>
2023-01-11 22:26:53 +0100

    feat: add link to github

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,23 @@
 <script>
   import ExternalLink from '$lib/components/ExternalLink.svelte';
+  import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
 </script>
 
 <footer class="text-white mt-4 py-6 bg-[#6f735f]">
-  <div class="common-width flex flex-row">
+  <div class="common-width flex flex-row flex-wrap gap-4">
     <div>© ArtSci 2023</div>
     <div class="flex-1" />
-    <div class="px-2">
+    <div class="">
       Visit our <ExternalLink href="https://www.facebook.com/ArtSci.ETHZ/">Facebook</ExternalLink>
     </div>
-    <div class="px-2">
+    <div class="">
       Visit our <ExternalLink href="https://twitter.com/artsci_ethz">Twitter</ExternalLink>
     </div>
-    <div class="px-2">
+    <div class="">
       Visit our <ExternalLink href="https://www.instagram.com/artsci.ethz/">Instagram</ExternalLink>
+    </div>
+    <div class="w-6">
+      <ExternalLink href="https://github.com/ArtSci-ethz"><FaGithub /></ExternalLink>
     </div>
   </div>
 </footer>
